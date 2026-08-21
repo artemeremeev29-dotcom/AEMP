b1ee1e58cfe62ddf352a3b195f9a006b7c4bdb24
artemeremeev29-dotcom <artemeremeev29@gmail.com>
2026-08-21 14:35:20 +1100

    Create AempMediaPlugin.java

---
+++ artifacts/music-player/android/app/src/main/java/com/aemp/musicplayer/AempMediaPlugin.java
@@ -0,0 +1,8 @@
+package com.aemp.musicplayer;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "AempMedia")
+public class AempMediaPlugin extends Plugin {
+}
