8eb9d70d8062e0f4e8afdd1ce651cf6d3f8b4739
artemeremeev29-dotcom <artemeremeev29@gmail.com>
2026-08-21 15:54:22 +1100

    Update AempMediaPlugin.java

--- artifacts/music-player/android/app/src/main/java/com/aemp/musicplayer/AempMediaPlugin.java
+++ artifacts/music-player/android/app/src/main/java/com/aemp/musicplayer/AempMediaPlugin.java
@@ -27,6 +27,27 @@ public class AempMediaPlugin extends Plugin {
     }
 
     @PluginMethod
+    public void updateMetadata(PluginCall call) {
+        String title = call.getString("title", "AEMP");
+        String artist = call.getString("artist", "AEMP");
+
+        Intent intent = new Intent(getContext(), AempAudioService.class);
+        intent.setAction(AempAudioService.ACTION_UPDATE_METADATA);
+        intent.putExtra(AempAudioService.EXTRA_TITLE, title);
+        intent.putExtra(AempAudioService.EXTRA_ARTIST, artist);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            getContext().startForegroundService(intent);
+        } else {
+            getContext().startService(intent);
+        }
+
+        JSObject result = new JSObject();
+        result.put("updated", true);
+        call.resolve(result);
+    }
+
+    @PluginMethod
     public void stopPlaybackService(PluginCall call) {
         Intent intent = new Intent(getContext(), AempAudioService.class);
         getContext().stopService(intent);
